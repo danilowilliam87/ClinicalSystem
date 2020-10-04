d1d2ef9cdcc4b564b8a63e74cd71a2e0c6bff468
danilowilliam87 <danilowilliam87@outlook.com>
2020-10-04 16:14:21 -0300

    Criada classe EnderecoService

--- src/main/java/io/github/danilowilliam/ClinicalSystem/service/EnderecoService.java
+++ src/main/java/io/github/danilowilliam/ClinicalSystem/service/EnderecoService.java
@@ -1,4 +1,49 @@
 package io.github.danilowilliam.ClinicalSystem.service;
 
+import io.github.danilowilliam.ClinicalSystem.model.Endereco;
+import io.github.danilowilliam.ClinicalSystem.repository.EnderecoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class EnderecoService {
+
+    @Autowired
+    private EnderecoRepository repository;
+
+    public List<Endereco>listarTodos(){
+        return repository.findAll();
+    }
+    public Optional<Endereco>buscaPorId(Long id){
+        return repository.findById(id);
+    }
+
+    public Endereco salvar(Endereco endereco){
+        return repository.save(endereco);
+    }
+
+    public boolean atualizar(Long id, Endereco endereco){
+        Optional<Endereco> antigo = repository.findById(id);
+        if(antigo.isPresent()){
+            Endereco novo = antigo.get();
+            novo.setCep(endereco.getCep());
+            novo.setLogradouro(endereco.getLogradouro());
+            novo.setNumero(endereco.getNumero());
+            novo.setCidadde(endereco.getCidadde());
+            novo.setEstado(endereco.getEstado());
+            repository.save(novo);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean deletar(Long id){
+        Optional<Endereco>endereco = repository.findById(id);
+        endereco.ifPresent(result -> repository.delete(result));
+        return true;
+    }
 }
