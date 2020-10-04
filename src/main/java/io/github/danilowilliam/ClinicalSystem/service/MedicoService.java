16fb6d83e9bf46b0b55df2e3f480574cff55690a
danilowilliam87 <danilowilliam87@outlook.com>
2020-10-04 16:23:45 -0300

    Criada classe MedicoService

--- src/main/java/io/github/danilowilliam/ClinicalSystem/service/MedicoService.java
+++ src/main/java/io/github/danilowilliam/ClinicalSystem/service/MedicoService.java
@@ -1,4 +1,46 @@
 package io.github.danilowilliam.ClinicalSystem.service;
 
+import io.github.danilowilliam.ClinicalSystem.model.Medico;
+import io.github.danilowilliam.ClinicalSystem.repository.MedicoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class MedicoService {
+
+    private MedicoRepository repository;
+
+    public List<Medico>listarTodos(){
+        return repository.findAll();
+    }
+    public Optional<Medico>buscarPoId(Long id){
+        return repository.findById(id);
+    }
+    public Medico salvar(Medico medico){
+        return repository.save(medico);
+    }
+    public boolean atualizar(Long id, Medico medico){
+        Optional<Medico>busca = repository.findById(id);
+        if (busca.isPresent()){
+            Medico novo = busca.get();
+            novo.setNome(medico.getNome());
+            novo.setCpf(medico.getCpf());
+            novo.setCrm(medico.getCrm());
+            novo.setEspecialidade(medico.getEspecialidade());
+            novo.setSenha(medico.getSenha());
+            novo.setConsultas(medico.getConsultas());
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean deletar(Long id){
+        Optional<Medico>medico = repository.findById(id);
+        medico.ifPresent(busca -> repository.delete(busca));
+        return true;
+    }
+
 }
