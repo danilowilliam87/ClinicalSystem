b0b935af8658d58603e058f67bdb687b45f37feb
danilowilliam87 <danilowilliam87@outlook.com>
2020-10-04 17:00:41 -0300

    Classe Consulta Service ainda não feita

--- src/main/java/io/github/danilowilliam/ClinicalSystem/service/ConsultaService.java
+++ src/main/java/io/github/danilowilliam/ClinicalSystem/service/ConsultaService.java
@@ -1,4 +1,13 @@
 package io.github.danilowilliam.ClinicalSystem.service;
 
+import io.github.danilowilliam.ClinicalSystem.repository.ConsultaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ConsultaService {
+
+    @Autowired
+    private ConsultaRepository repository;
+
 }
