96c9ae0c32d12fb72f8265586964c387d50784c5
danilowilliam87 <danilowilliam87@outlook.com>
2020-11-08 06:38:03 -0300

    Criado os meodos get lista e get dependente

--- src/main/java/io/github/danilowilliam/ClinicalSystem/controller/DependenteController.java
+++ src/main/java/io/github/danilowilliam/ClinicalSystem/controller/DependenteController.java
@@ -1,4 +1,36 @@
 package io.github.danilowilliam.ClinicalSystem.controller;
 
+import io.github.danilowilliam.ClinicalSystem.model.Dependente;
+import io.github.danilowilliam.ClinicalSystem.service.DependenteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/dependente")
 public class DependenteController {
+
+    @Autowired
+    DependenteService service;
+
+    //listar todos os dependentes
+    @GetMapping("/lista")
+    @ResponseBody
+    public List<Dependente>listar(){
+        return service.listarTodos();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Dependente>findById(@PathVariable("id")Long id){
+        Optional<Dependente>procurado = service.buscaPorId(id);
+        if (procurado.isPresent()){
+            return new ResponseEntity<Dependente>(procurado.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>( HttpStatus.NOT_FOUND);
+        }
+    }
 }
--- src/main/java/io/github/danilowilliam/ClinicalSystem/service/DependenteService.java
+++ src/main/java/io/github/danilowilliam/ClinicalSystem/service/DependenteService.java
@@ -2,6 +2,7 @@ package io.github.danilowilliam.ClinicalSystem.service;
 
 import io.github.danilowilliam.ClinicalSystem.model.Dependente;
 import io.github.danilowilliam.ClinicalSystem.repository.DependenteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,6 +11,7 @@ import java.util.Optional;
 @Service
 public class DependenteService {
 
+    @Autowired
     private DependenteRepository repository;
 
     public List<Dependente> listarTodos(){
