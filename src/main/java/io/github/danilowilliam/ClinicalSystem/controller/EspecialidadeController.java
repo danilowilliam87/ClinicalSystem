9ae88fef0275dddcbbd25a3f92dfc3c65a63a140
danilowilliam87 <danilowilliam87@outlook.com>
2020-11-21 19:26:45 -0300

    controller especialidade funcionando

--- src/main/java/io/github/danilowilliam/ClinicalSystem/controller/EspecialidadeController.java
+++ src/main/java/io/github/danilowilliam/ClinicalSystem/controller/EspecialidadeController.java
@@ -1,11 +1,13 @@
 package io.github.danilowilliam.ClinicalSystem.controller;
 
 import io.github.danilowilliam.ClinicalSystem.model.Especialidade;
+import io.github.danilowilliam.ClinicalSystem.repository.EspecialidadeRepository;
 import io.github.danilowilliam.ClinicalSystem.service.EspecialidadeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,45 +17,57 @@ import java.util.Optional;
 public class EspecialidadeController {
 
     @Autowired
-    private EspecialidadeService service;
+    private EspecialidadeRepository repository;
 
-    @GetMapping("/lista")
-    public List<Especialidade>listar(){
-        return service.listarTodos();
+    @ResponseBody
+    @PostMapping("/salvar")
+    public Especialidade salvar(@RequestBody Especialidade especialidade){
+        return repository.save(especialidade);
     }
 
     @GetMapping("/busca/{id}")
-    public ResponseEntity<Especialidade>buscaPorId(@PathVariable("id")Long id){
-        Optional<Especialidade>procurado = service.buscaPorId(id);
-        if (procurado.isPresent()){
-            return new ResponseEntity<Especialidade>(procurado.get(), HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+    @ResponseBody
+    public Especialidade busca(@PathVariable Long id){
+        return repository.
+                findById(id)
+                .orElseThrow(() -> new ResponseStatusException((HttpStatus.NOT_FOUND)));
+    }
+
+    @GetMapping("/busca-nome/{nome}")
+    @ResponseBody
+    public Especialidade buscaPorNome(@PathVariable String nome){
+        return repository
+                .findByNomeLike(nome)
+                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @GetMapping("/lista")
+    @ResponseBody
+    public List<Especialidade>lista(){
+        return repository.findAll();
     }
 
     @PutMapping("/atualizar/{id}")
-    public ResponseEntity<Especialidade>atualizar(@PathVariable("id") Long id,
-                                                  @RequestBody Especialidade especialidade){
-        if(service.atualizar(id, especialidade)){
-            return new ResponseEntity<Especialidade>(especialidade,HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
-        }
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void atualizar(@PathVariable Long id, @RequestBody Especialidade especialidadeatualizada){
+        repository
+                .findById(id)
+                .map(especialidade1 -> {
+                    especialidade1.setId(id);
+                    especialidade1.setNome(especialidadeatualizada.getNome());
+                    repository.save(especialidade1);
+                    return Void.TYPE;
+                }).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
-    @PostMapping("/salvar")
-    @ResponseBody
-    public Especialidade salvar(@RequestBody Especialidade especialidade){
-        return service.salvar(especialidade);
-    }
-
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/deletar/{id}")
-    public ResponseEntity<Especialidade>deletar(@PathVariable("id")Long id){
-        if (service.deletar(id)){
-            return new ResponseEntity<>(HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
-        }
+    public void delete(@PathVariable Long id){
+        repository
+                .findById(id)
+                .map(especialidade -> {
+                    repository.delete(especialidade);
+                    return Void.TYPE;
+                }).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 }
--- src/main/java/io/github/danilowilliam/ClinicalSystem/repository/EspecialidadeRepository.java
+++ src/main/java/io/github/danilowilliam/ClinicalSystem/repository/EspecialidadeRepository.java
@@ -4,6 +4,10 @@ import io.github.danilowilliam.ClinicalSystem.model.Especialidade;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface EspecialidadeRepository extends JpaRepository<Especialidade, Long> {
+
+    public Optional<Especialidade>findByNomeLike(String nome);
 }
