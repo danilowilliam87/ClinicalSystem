3a8e79d8f3a053ffb64eea346644d4e755295838
danilowilliam87 <danilowilliam87@outlook.com>
2021-02-21 14:23:40 -0300

    Retirada das Regras de Négocios da classe MedicoController

--- src/main/java/io/github/danilowilliam/ClinicalSystem/services/MedicoService.java
+++ src/main/java/io/github/danilowilliam/ClinicalSystem/services/MedicoService.java
@@ -1,9 +1,12 @@
 package io.github.danilowilliam.ClinicalSystem.services;
 
+import io.github.danilowilliam.ClinicalSystem.dto.response.MedicoResponseDTO;
 import io.github.danilowilliam.ClinicalSystem.model.Medico;
 import io.github.danilowilliam.ClinicalSystem.repository.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,35 +17,59 @@ public class MedicoService {
     @Autowired
     private MedicoRepository repository;
 
-    public List<Medico>listarTodos(){
-        return repository.findAll();
-    }
-    public Optional<Medico>buscarPoId(Long id){
-        return repository.findById(id);
-    }
+
     public Medico salvar(Medico medico){
+        Optional<Medico> busca = repository.findByCrmLike(medico.getCrm());
+        if(busca.isPresent()){
+            busca.map(atualizarMedico -> {
+                atualizarMedico.setNome(medico.getNome());
+                atualizarMedico.setCpf(medico.getCpf());
+                atualizarMedico.setCrm(medico.getCrm());
+                atualizarMedico.setEspecialidade(medico.getEspecialidade());
+                return repository.save(atualizarMedico);
+            });
+        }
         return repository.save(medico);
     }
-    public boolean atualizar(Long id, Medico medico){
-        Optional<Medico>busca = repository.findById(id);
-        if (busca.isPresent()){
-            Medico novo = busca.get();
-            novo.setNome(medico.getNome());
-            novo.setCpf(medico.getCpf());
-            novo.setCrm(medico.getCrm());
-            novo.setEspecialidade(medico.getEspecialidade());
-            novo.setSenha(medico.getSenha());
-            novo.setConsultas(medico.getConsultas());
-            return true;
-        } else {
-            return false;
-        }
+
+    public Medico busca(Long id){
+        return repository
+                .findById(id)
+                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
-    public boolean deletar(Long id){
-        Optional<Medico>medico = repository.findById(id);
-        medico.ifPresent(busca -> repository.delete(busca));
-        return true;
+    public Medico busca(String crm){
+        return repository
+                .findByCrmLike(crm)
+                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
+    public void atualizacaoParcial(Medico medico, Long id){
+        repository
+                .findById(id)
+                .map(atualizarMedico -> {
+                    atualizarMedico.setId(id);
+                    atualizarMedico.setNome(Optional.ofNullable(medico.getNome())
+                    .orElse(atualizarMedico.getNome()));
+                    atualizarMedico.setCrm(Optional.ofNullable(medico.getCrm())
+                    .orElse(atualizarMedico.getCrm()));
+                    atualizarMedico.setCpf(Optional.ofNullable(medico.getCrm())
+                    .orElse(atualizarMedico.getCpf()));
+                    atualizarMedico.setEspecialidade(Optional.ofNullable(medico.getEspecialidade())
+                    .orElse(atualizarMedico.getEspecialidade()));
+                    atualizarMedico.setSenha(Optional.ofNullable(medico.getSenha())
+                    .orElse(atualizarMedico.getSenha()));
+                    repository.save(atualizarMedico);
+                    return Void.TYPE;
+                })
+                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    public List<Medico>listar(){
+        return repository.findAll();
+    }
+
+    public void deletar(Long id){
+        repository.delete(busca(id));
+    }
 }
